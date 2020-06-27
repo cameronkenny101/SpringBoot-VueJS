b3d7b2b37a277590de4139257459d77214dc4545
cameronkenny101 <55995394+cameronkenny101@users.noreply.github.com>
2020-06-27 11:31:17 +0100

    Working Frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Hello</router-link> |
+      <router-link to="/callservice">Service</router-link> |
+      <router-link to="/bootstrap">Bootstrap</router-link> |
+      <router-link to="/user">User</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/protected">Protected</router-link>
+    </div>
+    <router-view :hellomsg="msg"></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to your Vue.js powered Spring Boot App'
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+     color: #42b983;
+    }
+  }
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Bootstrap
+Vue.use(BootstrapVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
+
